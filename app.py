59109cabcd98a910023ef6b2ebf933b07911f093
Noah De Marco <noahdicampolieto@gmail.com>
2025-08-07 18:43:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 app = Flask(__name__)
 
@@ -102,4 +103,7 @@ def index():
     '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
